eda11ab39195994936c3026da38079a3146cb538
kivisfr <kivisfriend@gmail.com>
2025-06-10 20:46:48 +0300

    git commit

--- src/main/java/com/attemptatadequatework/attempt_at_adequate_work/ControllerSettings.java
+++ src/main/java/com/attemptatadequatework/attempt_at_adequate_work/ControllerSettings.java
@@ -6,18 +6,44 @@ package com.attemptatadequatework.attempt_at_adequate_work;
      */
 
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.HBox;
+
+import java.sql.SQLException;
 
 public class ControllerSettings {
 
     @FXML
-    private Button  activatedButtonOne, activatedButtonTwo,
-                    activatedButtonThree, activatedButtonFour,
-                    activatedButtonFive;
+    private Button  activatedTableButton, activatedButtonTwo;
+
+    @FXML
+    public TextArea textArea;
+
+    @FXML
+    private HBox StandardHBox;
+
+    @FXML
+    private TableView StandardTable;
+
 
     @FXML
     protected void onButtonClick(String tableName) {
+        textArea.setText("Selected table " + "test_table" + " .");
 
+        try {
+            DataBasePart.table_view("test_table");
+
+            TableView tableView = DataBasePart.tableView;
+
+            int tableIndex = StandardHBox.getChildren().indexOf(StandardTable);
+            StandardHBox.getChildren().set(tableIndex, tableView);
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 
